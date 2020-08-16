e030de1050e8e38665bc246c3c41269d397c2d5a
Nils-Inge <nils-inge@bitnet.nu>
2020-08-16 23:02:18 +0200

    Very simple example data

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,12 +1,28 @@
 <script>
 	export let name;
-	let name2 = "Universe";
+	export let fillerData;
+	let name2 = "Natufian";
+	let name3 = "Tokharian";
+	let imgSource = "NatufianSpread.svg"; // bind with brackets
+	let src = "Andronovo_culture.png"; // same name as attribute == can use shorthand!!!
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<h1>Hello {name2}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{name2}!</h1>
+	<p>Visit Wikipedia to learn more about the
+		<a href="https://en.wikipedia.org/wiki/Natufian_culture">Natufian civilization</a>.
+	</p>
+	<img class="info-image" alt="spread of natufian culture" src={imgSource}>
+	<p>{fillerData}</p>
+
+	<h1>{name3}!</h1>
+	<p>Visit Wikipedia to learn more about the
+		<a href="https://en.wikipedia.org/wiki/Natufian_culture">Tokharian civilization</a>.
+	</p>
+	<img class="info-image" alt="spread of tokhraian culture" {src}> <!-- Short hand !-->
+	<p>{fillerData}</p>
+
+
 </main>
 
 <style>
@@ -28,5 +44,10 @@
 		main {
 			max-width: none;
 		}
+	}
+
+	.info-image {
+		border: solid 1px black;
+		border-radius: 2px;
 	}
 </style>
--- my-svelte-project/src/main.js
+++ my-svelte-project/src/main.js
@@ -4,6 +4,7 @@ const app = new App({
 	target: document.body,
 	props: {
 		name: 'world',
+		fillerData: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur aliquet diam eu molestie. Proin at rhoncus metus. Sed sit amet pulvinar neque, nec pulvinar risus. Praesent tristique tristique hendrerit. Curabitur suscipit hendrerit elit, ac tincidunt sem condimentum hendrerit. Nullam convallis ligula ac nunc suscipit laoreet. Morbi dapibus, enim vitae commodo maximus, lectus metus suscipit erat, eu porta augue nulla efficitur metus. Fusce vitae nunc lorem. Phasellus vestibulum, est vitae sagittis vulputate, odio metus dictum massa, id consectetur purus odio at mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus viverra risus metus, ut scelerisque sapien tincidunt eget. Aenean ultricies porta ex scelerisque pellentesque.'
 	}
 });
 
